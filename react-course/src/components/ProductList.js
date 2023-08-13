9344c623edaa3a7e6279c3c82988328d38ffbcef
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-14 03:12:12 +0500

    website that add and remove products item and gives its total

--- react-course/src/components/ProductList.js
+++ react-course/src/components/ProductList.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Product from './Product.js'
+import React from "react";
+import Product from "./Product.js";
 // rfc react functional component that gives whole function
-export default function ProductList(props) { //we cant change props
-    
-  return (
-    props.productList.map((product,i)=>{
-        return <Product product={product} key={i} incrementQuantity={props.incrementQuantity} index={i} decrementQuantity={props.decrementQuantity}/>
+export default function ProductList(props) {
+  //we cant change props
+
+  return props.productList.length > 0 ? (
+    props.productList.map((product, i) => {
+      return (
+        <Product
+          product={product}
+          key={i}
+          incrementQuantity={props.incrementQuantity}
+          index={i}
+          decrementQuantity={props.decrementQuantity}
+          removeItem={props.removeItem}
+        />
+      );
     })
-  )
+  ) : (
+    <h1>No products exist please ADD!</h1>
+  );
 }
